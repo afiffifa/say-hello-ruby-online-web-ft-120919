8bacb9cdb620891774473e9acb29a6c9e5d2eebe
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-21 17:09:10 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,11 @@
 # Build your say_hello method here
 def say_hello(name = "Ruby Programmer")
   puts "Hello, #{name}!"
+<<<<<<< HEAD
 end
 expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()	    say_hello("Ruby Programmer")
   end	 
+=======
+>>>>>>> 57f70e9003173064bcd8e5c74e44bc945741790d
 end
